513ddf251554a084b22650bd55539e7d3b29e32c
abrahanalarcon <moiseshazim@gmail.com>
2025-06-18 17:51:18 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SwitchTheme
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace SwitchTheme
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        bool isDarkMode = false; 
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void OnThemeToggled(object sender, ToggledEventArgs e)
+        {
+            isDarkMode = e.Value;
+            ApplyTheme(isDarkMode);
+
+            
+        }
+
+        private void ApplyTheme(bool darkMode)
+        {
+            var resources = Application.Current.Resources;
+
+            if (darkMode)
+            {
+                resources["DynamicPrimaryColor"] = resources["PrimaryColorDark"];
+                resources["DynamicSecondaryColor"] = resources["SecondaryColorDark"];
+                resources["DynamicTextColor"] = resources["TextColorDark"];
+                resources["DynamicBackgroundColor"] = resources["BackgroundColorDark"];
+            }
+            else
+            {
+                resources["DynamicPrimaryColor"] = resources["PrimaryColor"];
+                resources["DynamicSecondaryColor"] = resources["SecondaryColor"];
+                resources["DynamicTextColor"] = resources["TextColor"];
+                resources["DynamicBackgroundColor"] = resources["BackgroundColor"];
+            }
+        }
+    }
+}
